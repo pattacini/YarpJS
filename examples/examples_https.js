3303cbdd68d937e42f1d8531a662bc74f04f3ef1
Nuno Guedelha <nuno.guedelha@iit.it>
2021-06-10 03:37:55 +0200

    Improved Simple Example
    - Added link to "Simple Example" in the main page.
    - Minor refactoring of the https version of the js.
    - Fixed the style and the display of the port
      '/yarpjs/simple_example:i' input data.

--- examples/examples_https.js
+++ examples/examples_https.js
@@ -1,15 +1,26 @@
+// examples_https.js
 
-
-
-
-
-var https = require('https');
-var fs = require('fs');
-var express = require('express');
-
+// require yarp.js
 var yarp = require('../yarp');
 
+// setup static folders
+var express = require('express');
+var app = express();
+app.use(express.static('node_modules'));
+app.use(express.static('js'));
+app.use(express.static('examples'));
 
+// setup default page
+app.get('/', function(req, res){
+  res.sendFile('index.html',{ root : __dirname});
+});
+app.get('/yarp.js', function(req, res){
+  res.sendfile('/yarp.js',{ root : __dirname});
+});
+
+// provided a self-signed certificate to run local
+// applications from an HTTPS domain
+var fs = require('fs');
 var options = {
     key: fs.readFileSync('./examples/https/conf/server.key'),
     cert: fs.readFileSync('./examples/https/conf/server.crt'),
@@ -17,44 +28,14 @@ var options = {
     rejectUnauthorized: false
 };
 
+// require and setup basic http functionalities
+var https = require('https');
 
-var app = express();
-
-
-
-
-app.use(express.static('node_modules'));
-app.use(express.static('js'));
-app.use(express.static('examples'));
-
-
-app.get('/', function(req, res){
-  res.sendFile('index.html',{ root : __dirname});
-});
-
-
-app.get('/yarp.js', function(req, res){
-  res.sendfile('/yarp.js',{ root : __dirname});
-});
-
-
-
-
+// start the server!
 var server = https.createServer(options, app).listen(3000, function(){
     console.log('listening on *:3000');
 });
 
+// setup the communication with the browser using websockets
 var io = require('socket.io')(server);
-
-
 yarp.browserCommunicator(io);
-
-
-
-
-
-
-
-
-
-
